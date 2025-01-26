db5c41e9aa257bfe335e0f11a5b27f49c0014392
Alice <aliceblack@westernwyoming.edu>
2025-01-26 22:43:33 +0000

    Add files via upload

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
-// Hello World Programming Project
+// Alice Black
+// 2025JAN26
+// Your First C++ Program
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, world!";
+    return 0;
+}
