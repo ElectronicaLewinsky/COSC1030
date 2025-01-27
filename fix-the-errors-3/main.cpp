c23666dcfa28c8276d104028009b16babf79dd8e
Alice <aliceblack@westernwyoming.edu>
2025-01-27 01:39:12 +0000

    Update main.cpp

--- fix-the-errors-3/main.cpp
+++ fix-the-errors-3/main.cpp
@@ -1,6 +1,6 @@
 //
-// Name
-// Date
+// Alice Black
+// 2025JAN26
 // Fix the Errors #3 Programming Project
 // COSC 1030
 //
@@ -9,33 +9,33 @@
 // before printing the values. The user will be prompted to enter
 // both numbers.
 
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
-	float firstNumber;
-	float secondNumber;
-
-	// Prompt user to enter the first number.
+	float a;
+	float b;
+	float temp;
+	// Prompt user to enter the first number
 	cout << "Enter the first number" << endl;
 	cout << "Then hit enter" << endl;
-	cin >> firstNumber;
-
-	// Prompt user to enter the second number.
+	cin >> a;
+	// Prompt user to enter the second number
 	cout << "Enter the second number" << endl;
 	cout << "Then hit enter" << endl;
-	cin >> secondNumber;
+	cin >> b;
+	// Echo print the input
+    cout << "You input the numbers as" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+	// Now we swap the variables
+    temp = a;
+    a = b;
+    b = temp;
+	// Output the values
+    cout << "\nAfter swapping, the values are " << endl;
+    cout << "a = " << a << ", b = " << b << endl;
 
-	// Echo print the input.
-	cout << endl << "You input the numbers as " << firstNumber
-		<< " and " << secondNumber << endl;
-
-	// Now we will swap the values.
-	firstNumber = secondNumber; secondNumber = firstNumber;
-
-	// Output the values.
-	cout << "After swapping, the values of the two numbers are "
-		<< firstNumber << " and " << secondNumber << endl; return 0;
+    return 0;
 }
