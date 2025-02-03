774c5e4b38aef7904f91b58234e9a102f512032a
Alice <aliceblack@westernwyoming.edu>
2025-02-03 05:53:05 +0000

    Add files via upload

--- ocean-levels/main.cpp
+++ ocean-levels/main.cpp
@@ -1,8 +1,24 @@
 //
-// Name
-// Date
+// Alice Black 
+// 2025FEB02
 // Ocean Levels Programming Project
 // COSC 1030
 //
-
-
+#include <iostream>
+using namespace std;
+// Variable declaration and constants
+int main()
+{
+    const double MIL_PER_YEAR = 1.5;
+// Calculate sea level rise
+    cout << endl;
+    cout << "The ocean's level will rise " << MIL_PER_YEAR * 5;
+    cout << " millimeters after 5 years." << endl;
+    cout << "The ocean's level will rise " << MIL_PER_YEAR * 7;
+    cout << " millimeters after 7 years." << endl;
+    cout << "The ocean's level will rise " << MIL_PER_YEAR * 10;
+    cout << " millimeters after 10 years." << endl;
+    cout << endl;
+// Print results
+    return 0;
+}
