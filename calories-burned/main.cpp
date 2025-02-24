8fdb3d7a1253daf731cae8e163dd3c019c9ad296
Alice <aliceblack@westernwyoming.edu>
2025-02-24 04:15:15 +0000

    Update main.cpp

--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,24 @@
 //
-// Name
-// Date
+// Alice Black 
+// 2025FEB22
 // Calories Burned Programming Project
 // COSC 1030
 //
-
+#include <iostream>
+using namespace std;
+int main()
+{
+// Variable declaration
+    const float CALORIES_PER_MINUTE = 3.6;
+    float CALORIES_BURNED = 0;
+    for (int i = 5; i <= 30; i +=5)
+// Calculate and display result
+    {
+        CALORIES_BURNED = (i * CALORIES_PER_MINUTE);
+        cout << "Calories burned after" << i
+             << "minutes = "
+             << CALORIES_BURNED
+             << endl;
+    }
+    return 0;
+}
