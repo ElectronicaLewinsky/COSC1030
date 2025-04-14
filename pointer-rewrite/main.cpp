52a03ce7f20024d91e5fbc4571ca98f3e121ffb8
Alice <aliceblack@westernwyoming.edu>
2025-04-14 01:58:08 +0000

    Update main.cpp

--- pointer-rewrite/main.cpp
+++ pointer-rewrite/main.cpp
@@ -1,7 +1,36 @@
 //
-// Name
-// Date
+// Alice Black
+// 13APR2025
 // Pointer Rewrite Programming Project
 // COSC 1030
 //
+#include <iostream>
+using namespace std;
 
+// Function Header
+int doSomething(int *, int *);
+
+// Variables for doSomething function
+int main()
+{
+    int  z = 2,
+        *x = &z,
+        *y = &z;
+         z = doSomething(x, y);
+
+    cout << "z = " << z << endl;
+
+    x = nullptr;
+    z = nullptr;
+
+    return 0;
+}
+
+// Execute function
+int doSomething(int *x, int *y) 
+{
+    int temp = *x; 
+    *x = *y * 10;
+    *y = temp * 10; 
+    return *x + *y;
+}
